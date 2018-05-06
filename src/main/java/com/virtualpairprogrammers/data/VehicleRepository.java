3e53cc415d35d75e79289857452d09f3a531660d
Henrik Åman <softwareuabhnk@gmail.com>
2018-05-06 22:50:54 +0200

    vpp Spring BOOT - chapter 4 'JPA and Spring DATA' - Ongoing

---
+++ src/main/java/com/virtualpairprogrammers/data/VehicleRepository.java
@@ -0,0 +1,11 @@
+package com.virtualpairprogrammers.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.virtualpairprogrammers.domain.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>{
+	
+	
+
+}
---
+++ src/main/java/com/virtualpairprogrammers/domain/Vehicle.java
@@ -0,0 +1,22 @@
+package com.virtualpairprogrammers.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicle {
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Id
+	private long id;
+	private String name;
+	private int odometer;
+	private String status;
+	private String latLong;
+	private String currentDriver;
+	
+	public String toString() {
+		return this.name + "odometer" + this.odometer;
+	}
+}
